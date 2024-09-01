6648fd42b0484c2f888f98371c41635250a69c89
meghanshagarwal <meghanshagarwal3110@gmail.com>
2024-09-01 19:07:13 +0530

    Initial commit

---
+++ cart/update.js
@@ -0,0 +1,15 @@
+{
+    "token": "Z2NwLWFzaWEtc291dGhlYXN0MTowMUo2QUM5QUYzWE1LRTkzRjg4SjM0RlM2VA?key=55554a408d7d74d05c64207a058eb892",
+    "note": "",
+    "attributes": {},
+    "original_total_price": 0,
+    "total_price": 0,
+    "total_discount": 0,
+    "total_weight": 0.0,
+    "item_count": 0,
+    "items": [],
+    "requires_shipping": false,
+    "currency": "INR",
+    "items_subtotal_price": 0,
+    "cart_level_discount_applications": []
+}
